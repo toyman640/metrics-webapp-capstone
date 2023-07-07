683de7c1907de1fceac7596e355b80b4a926a994
toyman640 <falakoadebayor@gmail.com>
2023-07-07 16:58:25 +0100

    Fix linters error

--- src/_test_/Countries.test.js
+++ src/_test_/Countries.test.js
@@ -7,22 +7,20 @@ import Countries from '../components/Countries';
 const mockStore = configureStore([]);
 
 jest.mock('axios', () => ({
-  get: jest.fn(() =>
-    Promise.resolve({
-      data: [
-        {
-          id: 1,
-          name: 'Country 1',
-          image: 'image-url-1',
-        },
-        {
-          id: 2,
-          name: 'Country 2',
-          image: 'image-url-2',
-        },
-      ],
-    })
-  ),
+  get: jest.fn(() => Promise.resolve({
+    data: [
+      {
+        id: 1,
+        name: 'Country 1',
+        image: 'image-url-1',
+      },
+      {
+        id: 2,
+        name: 'Country 2',
+        image: 'image-url-2',
+      },
+    ],
+  })),
 }));
 
 describe('Countries', () => {
@@ -42,7 +40,7 @@ describe('Countries', () => {
     const component = renderer.create(
       <Provider store={store}>
         <Countries />
-      </Provider>
+      </Provider>,
     );
     expect(component.toJSON()).toMatchSnapshot();
   });
